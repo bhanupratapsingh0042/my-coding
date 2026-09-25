2f87c53bfed3d1100615969e90aab6d959af9d62
Bhanu pratap Singh <bhanupratapsingh0042@gmail.com>
2026-09-25 19:58:46 +0530

    Add files via upload

---
+++ counting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("20 se 1 tak ki ulti ginti shuru ho rahi hai:\n");
+
+    // यहाँ हमने तीनों चीजें भर दी हैं
+    for(i = 20; i >= 1; i--) 
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
